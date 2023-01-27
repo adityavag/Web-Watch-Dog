46ad590a495708a7550f4ff802cf2c3dd0053ac7
adi247u <adi247u@gmail.com>
2023-01-27 22:55:34 +0530

    #1

---
+++ web-watch-dog/src/main/java/io/aditya/web/watch/dog/WebWatchDogApplication.java
@@ -0,0 +1,13 @@
+package io.aditya.web.watch.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWatchDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebWatchDogApplication.class, args);
+	}
+
+}
---
+++ web-watch-dog/src/test/java/io/aditya/web/watch/dog/WebWatchDogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aditya.web.watch.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWatchDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
