757313f399a00294c981c14252c7b1fd53f50c6c
adi247u <adi247u@gmail.com>
2023-03-09 23:13:39 +0530

    UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [userInput, setUserInput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-2xl font-medium text-blue-500 mb-4">
+          Web Watch Dog<span role="img" aria-label="dog"> 🐕</span>
+        </h1>
+        <div>
+          <input
+            className="outline-none border-2 border-blue-500 rounded-md backdrop-blur-xl bg-white/20 shadow-md px-3 py-3"
+            type="text"
+            placeholder="Enter URL"
+            value={userInput}
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+            />
+            <button 
+              className="bg-blue-500 text-white px-8 py-3 ml-4 rounded-md"
+              onClick={test}>
+              Submit
+            </button>
+            <div className="mt-5">
+              {/* {} */}
+            </div>
+        </div>
+      </div> 
     </div>
   );
 }
